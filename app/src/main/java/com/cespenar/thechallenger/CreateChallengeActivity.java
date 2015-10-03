4ee7d66122dd934b0c05616c9bd1c143de044bd3
Tomasz Jasianek <jasbuber@gmail.com>
2015-10-02 23:26:06 -0400

    Removed private challenges indefinitely

--- app/src/main/java/com/cespenar/thechallenger/CreateChallengeActivity.java
+++ app/src/main/java/com/cespenar/thechallenger/CreateChallengeActivity.java
@@ -10,6 +10,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
@@ -20,6 +21,12 @@ import com.cespenar.thechallenger.models.Challenge;
 import com.cespenar.thechallenger.models.CustomResponse;
 import com.cespenar.thechallenger.services.ChallengeService;
 import com.cespenar.thechallenger.services.FacebookService;
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.share.model.AppInviteContent;
+import com.facebook.share.widget.AppInviteDialog;
 
 import java.io.File;
 
@@ -32,10 +39,15 @@ public class CreateChallengeActivity extends Activity {
     private boolean isStep2Visible = false;
     private boolean isStep3Visible = false;
 
+    //private static final int VISIBILITY_PRIVATE = 1;
+    //private static final int VISIBILITY_PUBLIC = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_challenge);
+
+        //getChallengeVisibilityElement().setOnItemSelectedListener(getVisibilityListener(this));
 
         if (savedInstanceState != null) {
             if (savedInstanceState.getBoolean("isStep2Visible")) {
@@ -54,7 +66,7 @@ public class CreateChallengeActivity extends Activity {
             }
             getChallengeCategoryElement().setSelection(savedInstanceState.getInt("challengeCategory"));
             getChallengeDifficultyElement().setProgress(savedInstanceState.getInt("challengeDifficulty"));
-            getChallengeVisibilityElement().setSelection(savedInstanceState.getInt("challengeVisibility"));
+            //getChallengeVisibilityElement().setSelection(savedInstanceState.getInt("challengeVisibility"));
         }
     }
 
@@ -65,7 +77,7 @@ public class CreateChallengeActivity extends Activity {
         savedInstanceState.putInt("challengeCategory", getChallengeCategoryElement().getSelectedItemPosition());
         savedInstanceState.putString("challengeVideo", challengeVideo);
         savedInstanceState.putInt("challengeDifficulty", getChallengeDifficultyElement().getProgress());
-        savedInstanceState.putInt("challengeVisibility", getChallengeVisibilityElement().getSelectedItemPosition());
+        //savedInstanceState.putInt("challengeVisibility", getChallengeVisibilityElement().getSelectedItemPosition());
 
         savedInstanceState.putBoolean("isStep2Visible", isStep2Visible);
         savedInstanceState.putBoolean("isStep3Visible", isStep3Visible);
@@ -104,10 +116,11 @@ public class CreateChallengeActivity extends Activity {
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
 
         super.onActivityResult(requestCode, resultCode, data);
+        FacebookService.callbackManager.onActivityResult(requestCode, resultCode, data);
 
         if (resultCode == RESULT_OK) {
             if (requestCode == SELECT_VIDEO) {
-                challengeVideo = getPath(data.getData());
+                challengeVideo = getPath(this, data.getData());
                 File videoName = new File(challengeVideo);
 
                 getChallengeVideoElement().setText(videoName.getName());
@@ -116,11 +129,11 @@ public class CreateChallengeActivity extends Activity {
         }
     }
 
-    public String getPath(Uri uri) {
+    public static String getPath(Activity activity, Uri uri) {
 
         String result = null;
 
-        Cursor cursor = getContentResolver().query(uri, new String[]{MediaStore.Images.Media.DATA}, null, null, null);
+        Cursor cursor = activity.getContentResolver().query(uri, new String[]{MediaStore.Images.Media.DATA}, null, null, null);
         if (cursor.moveToFirst()) {
             int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
             result = cursor.getString(column_index);
@@ -154,10 +167,11 @@ public class CreateChallengeActivity extends Activity {
             view.setEnabled(false);
 
             String challengeCategory = (String) getChallengeCategoryElement().getSelectedItem();
-            boolean challengeVisibility = false;
+            boolean challengeVisibility = true;
+            /*
             if (getChallengeVisibilityElement().getSelectedItemPosition() == 0) {
                 challengeVisibility = true;
-            }
+            }*/
             int challengeDifficulty = getChallengeDifficultyElement().getProgress();
 
             Challenge challenge = new Challenge(challengeName, challengeVideo, challengeCategory,
@@ -191,13 +205,13 @@ public class CreateChallengeActivity extends Activity {
         return true;
     }
 
-    public void finalizeCreateChallenge(CustomResponse response){
+    public void finalizeCreateChallenge(CustomResponse response) {
 
-        if(response.getStatus() == CustomResponse.ResponseStatus.failure){
+        if (response.getStatus() == CustomResponse.ResponseStatus.failure) {
 
-            for(String message : response.getMessages()){
-                switch(message){
-                    case "no_participants" :
+            for (String message : response.getMessages()) {
+                switch (message) {
+                    case "no_participants":
                         break;
                 }
 
@@ -222,9 +236,9 @@ public class CreateChallengeActivity extends Activity {
         return (Spinner) findViewById(R.id.create_challenge_category);
     }
 
-    private Spinner getChallengeVisibilityElement() {
+    /*private Spinner getChallengeVisibilityElement() {
         return (Spinner) findViewById(R.id.create_challenge_visibility);
-    }
+    }*/
 
     private SeekBar getChallengeDifficultyElement() {
         return (SeekBar) findViewById(R.id.create_challenge_difficulty);
@@ -233,5 +247,64 @@ public class CreateChallengeActivity extends Activity {
     private Button getChallengeVideoElement() {
         return (Button) findViewById(R.id.create_challenge_upload);
     }
+    /*
+
+    private AdapterView.OnItemSelectedListener getVisibilityListener(final Activity activity) {
+        return new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                if (id == VISIBILITY_PRIVATE) {
+
+                    String appLinkUrl = "https://fb.me/681646071937347";
+                    String previewImageUrl = "https://nameless-badlands-7043.herokuapp.com/assets/images/glyph_glasses.png";
+                    final String TAG = "fbv4";
+
+                    if (AccessToken.getCurrentAccessToken() == null) {
+                        // start login...
+                    } else {
+                        FacebookService.callbackManager = CallbackManager.Factory.create();
+
+                        FacebookCallback<AppInviteDialog.Result> facebookCallback = new FacebookCallback<AppInviteDialog.Result>() {
+                            @Override
+                            public void onSuccess(AppInviteDialog.Result result) {
+                                Log.i(TAG, "MainACtivity, InviteCallback - SUCCESS!" + result.getData());
+                                Log.e("success", result.getData().toString());
+                            }
+
+                            @Override
+                            public void onCancel() {
+                                Log.i(TAG, "MainACtivity, InviteCallback - CANCEL!");
+                                Log.e("success", "cancel");
+                            }
+
+                            @Override
+                            public void onError(FacebookException e) {
+                                Log.e(TAG, "MainACtivity, InviteCallback - ERROR! " + e.getMessage());
+                                Log.e("error", e.getMessage());
+                            }
+                        };
+
+                        AppInviteDialog appInviteDialog = new AppInviteDialog(activity);
+                        if (AppInviteDialog.canShow()) {
+                            AppInviteContent.Builder content = new AppInviteContent.Builder();
+                            content.setApplinkUrl(appLinkUrl);
+                            content.setPreviewImageUrl(previewImageUrl);
+                            AppInviteContent appInviteContent = content.build();
+                            appInviteDialog.registerCallback(FacebookService.callbackManager, facebookCallback);
+                            AppInviteDialog.show(activity, appInviteContent);
+                        }
+
+                    }
+                } else {
+
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // your code here
+            }
+        };
+    }*/
 
 }
